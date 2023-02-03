47ca2577fcdddf96e5df9254ce5436d81b1a0c27
ShivamMishra828 <shivam9878522087@gmail.com>
2023-02-03 18:07:02 +0530

    Added Login and Register Page

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import DashboardLayout from "../layouts/dashboard";
 // config
 import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
+import MainLayout from "../layouts/main";
 
 const Loadable = (Component) => (props) => {
   return (
@@ -19,13 +20,24 @@ const Loadable = (Component) => (props) => {
 export default function Router() {
   return useRoutes([
     {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [
+        { element: <LoginPage />, path: "login" },
+        { element: <RegisterPage />, path: "register" },
+        { element: <ResetPassword />, path: "reset-password" },
+        { element: <NewPassword />, path: "new-password" },
+      ],
+    },
+
+    {
       path: "/",
       element: <DashboardLayout />,
       children: [
         { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
         { path: "app", element: <GeneralApp /> },
         { path: "settings", element: <Settings /> },
-        
+
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -35,9 +47,11 @@ export default function Router() {
 }
 
 const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
+  lazy(() => import("../pages/dashboard/GeneralApp"))
 );
-const Settings = Loadable(
-  lazy(() => import("../pages/dashboard/Settings")),
-);
+const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
+const RegisterPage = Loadable(lazy(() => import("../pages/auth/Register")));
+const ResetPassword = Loadable(lazy(() => import("../pages/auth/ResetPassword")));
+const NewPassword = Loadable(lazy(() => import("../pages/auth/NewPassword")));
+const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
